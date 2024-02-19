f994ca86d99b788da4dbbb7e96c8bd46c3b64552
David Aw <89636227+davidaw78@users.noreply.github.com>
2024-02-19 21:32:45 +0800

    Remove extra cidr block. Test theory

--- main.tf
+++ main.tf
@@ -30,13 +30,13 @@ variable "public-subnet-cidr-blocks" {
 
 variable "private-subnet-cidr-blocks-app" {
   type        = list(string)
-  default     = ["10.0.3.0/24", "10.0.4.0/24"]
+  default     = ["10.0.3.0/24"]
   description = "CIDR block range for private subnet"
 }
 
 variable "private-subnet-cidr-blocks-db" {
   type        = list(string)
-  default     = ["10.0.5.0/24", "10.0.6.0/24"]
+  default     = ["10.0.5.0/24"]
   description = "CIDR block range for private subnet"
 }
 
