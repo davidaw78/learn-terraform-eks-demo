22a26140cf3ab9cfb358116183bb88d5b4f5ed24
David Aw <89636227+davidawcloudsecurity@users.noreply.github.com>
2024-08-09 15:05:23 +0800

    Update main.tf - enable iam to allow cloudwatch

--- main.tf
+++ main.tf
@@ -631,25 +631,53 @@ resource "aws_launch_template" "terraform-eks-demo" {
 }
 
 # Create CloudWatch Log Group for VPC Flow Logs
-resource "aws_cloudwatch_log_group" "vpc_flow_logs" {
-  name = "${var.cluster-name}-vpc-flow-logs"
-  
-  retention_in_days = 7 # Optional: Define retention policy (7 days here)
-  
-  tags = {
-    Name = "${var.cluster-name}-vpc-flow-logs"
+resource "aws_flow_log" "example" {
+  iam_role_arn    = aws_iam_role.example.arn
+  log_destination = aws_cloudwatch_log_group.example.arn
+  traffic_type    = "ALL"
+  vpc_id          = aws_vpc.example.id
+}
+
+resource "aws_cloudwatch_log_group" "example" {
+  name = "example"
+}
+
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["vpc-flow-logs.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
   }
 }
 
-# Create VPC Flow Log
-resource "aws_flow_log" "vpc_flow_log" {
-  log_destination = aws_cloudwatch_log_group.vpc_flow_logs.name
-  traffic_type    = "ALL" # Can be "ACCEPT", "REJECT", or "ALL"
-
-  vpc_id = aws_vpc.terraform-eks-vpc.id
-
-  depends_on = [
-    aws_cloudwatch_log_group.vpc_flow_logs
-  ]
+resource "aws_iam_role" "example" {
+  name               = "example"
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
 }
 
+data "aws_iam_policy_document" "example" {
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "logs:CreateLogGroup",
+      "logs:CreateLogStream",
+      "logs:PutLogEvents",
+      "logs:DescribeLogGroups",
+      "logs:DescribeLogStreams",
+    ]
+
+    resources = ["*"]
+  }
+}
+
+resource "aws_iam_role_policy" "example" {
+  name   = "example"
+  role   = aws_iam_role.example.id
+  policy = data.aws_iam_policy_document.example.json
+}
